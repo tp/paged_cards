6f51e2d165dd60513c7ac24d4c926ef86b85a1c5
Timm Preetz <timm@preetz.us>
2019-04-11 07:23:07 +0200

    Initial setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# PagedCards
+
+Flutter widget to show a horizontal list of paged cards which can be swiped-through or scrolled up to transition to full width.
---
+++ lib/src/card.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class CardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      color: Colors.blue,
+    );
+  }
+}
